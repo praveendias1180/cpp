359eaee629f478e5cdc7edcaf06db94f8faa084d
Praveen Dias <praveendias1180@gmail.com>
2022-06-26 21:33:49 +0530

    comments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+/*
+ Multi line Comment
+ Second line
+ */
 int main()
 {
+    // One line comment
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello Again!" << std::endl;
     return 0;
